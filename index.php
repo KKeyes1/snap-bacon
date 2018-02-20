9413e050f5003f40fc5a4729ceaed0ee32dcc93e
Ken Keyes <kkeyes1@cnm.edu>
2018-02-20 08:47:28 -0700

    Copy and Pasted - it works...

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Snap Bacon</title>
+<!--		<script type="text/javascript" src="bacon.js"></script>-->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	</head>
+	<body>
+		<script>
+			$(document).ready(function()
+			{
+				$("#baconButton").click(function()
+				{
+					$.getJSON('https://baconipsum.com/api/?callback=?',
+						{ 'type':'meat-and-filler', 'start-with-lorem':'1', 'paras':'5' },
+						function(baconGoodness)
+						{
+							if (baconGoodness && baconGoodness.length > 0)
+							{
+								$("#baconIpsumOutput").html('');
+								for (var i = 0; i < baconGoodness.length; i++)
+									$("#baconIpsumOutput").append('<p>' + baconGoodness[i] + '</p>');
+								$("#baconIpsumOutput").show();
+							}
+						});
+				});
+			});
+		</script>
+		<input id="baconButton" type="button" value="Give me bacon">
+		<div id="baconIpsumOutput">
+			<p></p>
+		</div>
+	</body>
+</html>
